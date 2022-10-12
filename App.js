3f7dbf904cd66dfdf0c83d5a30511bc3434e3be9
Adrion10 <lacehilaQgmail.com>
2022-10-12 14:09:11 +0200

    fixed

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from "expo-status-bar";
+// import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World fron Lace!</Text>
-      <StatusBar style="auto" />
+      {/* <Text>Hello World fron Lace!</Text> */}
+      {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
