6fe02125a965bf2de345d10febae8d685fbcbadf
Adrion10 <lacehilaQgmail.com>
2022-10-17 18:45:11 +0200

    <ScrollView alwaysBounceVertical={false}>

--- App.js
+++ App.js
@@ -50,7 +50,14 @@
 // });
 
 import { useState } from "react";
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView,
+} from "react-native";
 
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState("");
@@ -79,11 +86,13 @@ export default function App() {
         <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        {courseGoals.map((goal) => (
-          <View key={goal} style={styles.goalItem}>
-            <Text style={styles.goalText}> {goal} </Text>
-          </View>
-        ))}
+        <ScrollView alwaysBounceVertical={false}>
+          {courseGoals.map((goal) => (
+            <View key={goal} style={styles.goalItem}>
+              <Text style={styles.goalText}> {goal} </Text>
+            </View>
+          ))}
+        </ScrollView>
       </View>
     </View>
   );
