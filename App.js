3648eeee721aa8f987905870b6b01c91e2d4c98e
Adrion10 <lacehilaQgmail.com>
2022-10-17 18:29:50 +0200

    fixed

--- App.js
+++ App.js
@@ -1,3 +1,54 @@
+// import { StatusBar } from "expo-status-bar";
+// import { StyleSheet, Text, View, Button } from "react-native";
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <View>
+//         <Text
+//           style={styles.dummyText}
+//           // style={{
+//           //   margin: 16,
+//           //   borderWidth: 2,
+//           //   borderColor: "red",
+//           //   padding: 16,
+//           // }}
+//         >
+//           Hello World from Lace!!!
+//         </Text>
+//       </View>
+
+//       <Text
+//         style={{
+//           margin: 16,
+//           borderWidth: 2,
+//           borderColor: "red",
+//           padding: 16,
+//         }}
+//       >
+//         Another piece of text
+//       </Text>
+//       <StatusBar style="auto" />
+//       <Button title="Click Me" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+//   dummyText: {
+//     margin: 10,
+//     padding: 10,
+//     borderWidth: 4,
+//     borderColor: "blue",
+//   },
+// });
+
 import { useState } from "react";
 import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
@@ -30,7 +81,7 @@ export default function App() {
       <View style={styles.goalsContainer}>
         {courseGoals.map((goal) => (
           <View key={goal} style={styles.goalItem}>
-            <Text> {goal} </Text>
+            <Text style={styles.goalText}> {goal} </Text>
           </View>
         ))}
       </View>
@@ -67,6 +118,8 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     padding: 8,
     color: "white",
+  },
+  goalText: {
     backgroundColor: "#cccccc",
   },
 });
